c1496d0093ed764e605626ddc1467657574ff67e
helping083 <helping083@gmail.com>
2019-06-05 17:17:11 +0300

    django init

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\олег\django-bootstrap\python-django-real-estate-app\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
