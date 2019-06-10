20983ddb2dd8301f75468157b508921c7ca6fe78
helping083 <helping083@gmail.com>
2019-06-10 12:10:09 +0300

    add dynamic and style the admin page

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,3 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+console.log('hello world from static main.js')
--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Listing
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id','title', 'price')
+    list_filter = ('id', 'title', 'price', 'list_date', 'realtor')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'adress', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 10
+admin.site.register(Listing, ListingAdmin)
